615f5f3c7f13e8efbc8b938b3331c6215c1a9ac0
ayu055 <ayushagarwal2500@gmail.com>
2022-06-29 13:19:52 -0400

    Added pandas test code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
 print("hello world")
+
+data = {
+    'age': list(range(10, 20)),
+    'height': list(range(50, 60))
+}
+
+df = pd.DataFrame(data, index=["p{}".format(idx) for idx in range(10)])
+
+print(df)
