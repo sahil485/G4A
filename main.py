6bbbd4f8d020e8437dee1de84a18b8bbe558bbef
ayush055 <ayushagarwal2500@gmail.com>
2022-07-01 10:09:56 -0400

    Face Recognition testing

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-import pandas as pd
+import face_recognition
+import pickle
+import cv2
+import os
 
-print("hello world")
+# imagePaths = list(paths.list_images('Images'))
+imageDir = 'images'
+imagePaths = os.listdir(imageDir)
 
-data = {
-    'age': list(range(10, 20)),
-    'height': list(range(50, 60))
-}
+knownEncodings = []
+knownNames = []
 
-df = pd.DataFrame(data, index=["p{}".format(idx) for idx in range(10)])
+for (i, imageName) in enumerate(imagePaths):
+    name = imageName.split('.')[0]
+    image = cv2.imread(os.path.join(imageDir, imageName))
+    print(image.shape)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (200, 200))
+    # print(image.shape)
 
-print(df)
+    # cv2.imshow('img', image)
+    # cv2.waitKey(5000)
+
+    boxes = face_recognition.face_locations(image, model='cnn')
+    print("Boxes:", boxes)
+
+    for box in boxes:
+        y1, x2, y2, x1 = box
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    
+    encodings = face_recognition.face_encodings(image, boxes)
+    print("Encodings:", encodings)
+
+    for encoding in encodings:  
+        knownEncodings.append(encoding)
+        knownNames.append(name)
+
+data = {"encodings": knownEncodings, "names": knownNames}
+
+print(data)
