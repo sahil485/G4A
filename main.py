db0d5f1eebd5c909dce269cd27aa799aace1bcb1
sillyhill-c <sahil.chatiwala@gmail.com>
2022-07-01 11:15:19 -0400

    implemented facial_recog/tested

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for (i, imageName) in enumerate(imagePaths):
     image = cv2.resize(image, (200, 200))
     # print(image.shape)
 
-    # cv2.imshow('img', image)
+    cv2.imshow('img', image)
     # cv2.waitKey(5000)
 
     boxes = face_recognition.face_locations(image, model='cnn')
@@ -27,9 +27,12 @@ for (i, imageName) in enumerate(imagePaths):
     for box in boxes:
         y1, x2, y2, x1 = box
         image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        print("here")
     
     cv2.imshow('image', image)
     cv2.waitKey(0)
+    cv2.destroyWindow("img")
+    cv2.destroyWindow("image")
     
     encodings = face_recognition.face_encodings(image, boxes)
     print("Encodings:", encodings)
