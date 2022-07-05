3d74bd6f685845b447c6930d9cc8062b199d589e
Ayush Agarwal <ayushagarwal2500@gmail.com>
2022-07-05 10:15:14 -0400

    Adding training and inference code

---
+++ encodings.py
@@ -0,0 +1,45 @@
+import face_recognition
+import pickle
+import cv2
+import os
+
+imageDir = 'images'
+imagePaths = os.listdir(imageDir)
+
+knownEncodings = []
+knownNames = []
+
+for (i, imageName) in enumerate(imagePaths):
+    name = imageName.split('.')[0].split('-')[0]
+    image = cv2.imread(os.path.join(imageDir, imageName))
+    # image = cv2.resize(image, (224, 224))
+
+    boxes = face_recognition.face_locations(image, model='cnn')
+    print("Boxes:", boxes)
+
+    encodings = face_recognition.face_encodings(image, boxes)
+    print("Encodings:", encodings)
+
+    # for encoding in encodings:  
+    #     knownEncodings.append(encoding)
+    #     knownNames.append(name)
+
+    # Add first encoding (probably for biggest face)
+    knownEncodings.append(encodings[0])
+    knownNames.append(name)
+
+    assert len(boxes) == len(encodings)
+    print("[INFO] Name: {} Number of Boxes: {} Number of Encodings: {}".format(name, len(boxes), len(encodings)))
+    
+    for box in boxes:
+        y1, x2, y2, x1 = box
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+
+data = {"encodings": knownEncodings, "names": knownNames}
+
+f = open("face_enc", "wb")
+f.write(pickle.dumps(data))
+f.close()
--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import face_recognition
 import pickle
+import time
 import cv2
 import os
 
-# imagePaths = list(paths.list_images('Images'))
-imageDir = 'images'
-imagePaths = os.listdir(imageDir)
+data = pickle.loads(open('face_enc', "rb").read())
+print(data)
 
-knownEncodings = []
-knownNames = []
+video_capture = cv2.VideoCapture(1)
+ret, image = video_capture.read()
 
-for (i, imageName) in enumerate(imagePaths):
-    name = imageName.split('.')[0]
-    image = cv2.imread(os.path.join(imageDir, imageName))
-    # image = cv2.resize(image, (224, 224))
+faces = face_recognition.face_locations(image, model='hog')
 
-    boxes = face_recognition.face_locations(image, model='hog')
-    print("Boxes:", boxes)
+# cascPathface = os.path.dirname(
+#  cv2.__file__) + "/data/haarcascade_frontalface_default.xml"
+# faceCascade = cv2.CascadeClassifier(cascPathface)
+# faces = faceCascade.detectMultiScale(image,
+#                                          scaleFactor=1.1,
+#                                          minNeighbors=5,
+#                                          minSize=(200, 200),
+#                                          flags=cv2.CASCADE_SCALE_IMAGE)
 
-    encodings = face_recognition.face_encodings(image, boxes)
-    print("Encodings:", encodings)
+encodings = face_recognition.face_encodings(image)
+names = []
 
-    for encoding in encodings:  
-        knownEncodings.append(encoding)
-        knownNames.append(name)
-    
-    for box in boxes:
-        y1, x2, y2, x1 = box
-        image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
+for encoding in encodings:
+    matches = face_recognition.compare_faces(data['encodings'], encoding)
+    name = "Unknown"
 
-data = {"encodings": knownEncodings, "names": knownNames}
+    if True in matches:
+        matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+        counts = {}
+        # loop over the matched indexes and maintain a count for
+        # each recognized face face
+        for i in matchedIdxs:
+            #Check the names at respective indexes we stored in matchedIdxs
+            name = data["names"][i]
+            #increase count for the name we got
+            counts[name] = counts.get(name, 0) + 1
+            #set name which has highest count
+        name = max(counts, key=counts.get)
 
-f = open("face_enc", "wb")
-f.write(pickle.dumps(data))
-f.close()
+        names.append(name)
+
+for ((y1, x2, y2, x1), name) in zip(faces, names):
+    # rescale the face coordinates
+    # draw the predicted face name on the image
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.putText(image, name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX,
+        0.75, (0, 255, 0), 2)
+
+cv2.imshow("Frame", image)
+cv2.waitKey(0)
