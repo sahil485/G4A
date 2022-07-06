fc843c888716b5c8ecb76cca5ec558dd7bee30ce
Ayush Agarwal <ayushagarwal2500@gmail.com>
2022-07-06 09:32:10 -0400

    Deleted wrong images and updated encodings.py

--- encodings.py
+++ encodings.py
@@ -14,18 +14,18 @@ for (i, imageName) in enumerate(imagePaths):
     image = cv2.imread(os.path.join(imageDir, imageName))
     # image = cv2.resize(image, (224, 224))
 
-    boxes = face_recognition.face_locations(image, model='hog')
-    print("Boxes:", boxes)
+    boxes = face_recognition.face_locations(image, model='cnn')
+    # print("Boxes:", boxes)
 
     encodings = face_recognition.face_encodings(image, boxes)
-    print("Encodings:", encodings)
+    # print("Encodings:", encodings)
 
     # for encoding in encodings:  
     #     knownEncodings.append(encoding)
     #     knownNames.append(name)
 
     assert len(boxes) == len(encodings)
-    print("[INFO] Name: {} Number of Boxes: {} Number of Encodings: {}".format(name, len(boxes), len(encodings)))
+    print("[INFO] Name: {} Number of Boxes: {} Number of Encodings: {}".format(imageName.split('.')[0], len(boxes), len(encodings)))
     
     # Add first encoding (probably for biggest face)
     knownEncodings.append(encodings[0])
@@ -35,8 +35,8 @@ for (i, imageName) in enumerate(imagePaths):
         y1, x2, y2, x1 = box
         image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
+    # cv2.imshow('image', image)
+    # cv2.waitKey(0)
 
 data = {"encodings": knownEncodings, "names": knownNames}
 
